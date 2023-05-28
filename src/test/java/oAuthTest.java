d6735591c668dfbb6638743de3d1428024048e80
Anit Rai <anitrai747@gmail.com>
2023-05-28 20:51:18 +0530

    commit

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -1,5 +1,7 @@
 import static io.restassured.RestAssured.*;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +9,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import Pojo.API;
+import Pojo.GetCourse;
+import Pojo.WebAutomation;
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 public class oAuthTest {
 	
@@ -23,7 +29,7 @@ public class oAuthTest {
 //		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Iamanitrai@747");
 //		driver.findElement(By.xpath("//input[@type='password']")).sendKeys(Keys.ENTER);
 //	String url =	driver.getCurrentUrl();
-		String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AbUR2VOL9NjwCUh28wMc6Fxj3oqwnNfGC2WJOw0A0Q1zVy74vnsdf32rs_cM53t1Hv_KMA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent";
+		String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AbUR2VPe0vbeJB9V8c0wAAReT-X9oDJ--H2ddNcwvbovVSkDBnQL9kTMBPpA2sVtBFMw5Q&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=none";
 	String partialCode = url.split("code=")[1];
 	String Code = partialCode.split("&scope")[0];
 	System.out.println(Code);
@@ -42,9 +48,26 @@ public class oAuthTest {
 		
 		
 		
-		String response = given().queryParam("access_token", accessToken).
-		when().get("https://rahulshettyacademy.com/getCourse.php").asString();
-		System.out.println(response);
+		GetCourse  gc = given().queryParam("access_token", accessToken).expect().defaultParser(Parser.JSON).
+		when().get("https://rahulshettyacademy.com/getCourse.php").as(GetCourse.class);
+		//System.out.println(response);
+		
+		System.out.println(gc.getLinkedIn());
+		System.out.println(gc.getCourses().getApi().get(1).getCourseTitle());
+		
+		List<API> apiCourses = gc.getCourses().getApi();
+		for(int i =0;i<apiCourses.size();i++) {
+			
+			if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")){
+				
+			System.out.println(apiCourses.get(i).getPrice());
+			}
+		}
+		
+		List<WebAutomation> webAutomation = gc.getCourses().getWebAutomation();
+		for (int i =0;i<webAutomation.size();i++) {
+	System.out.println(webAutomation.get(i).getCourseTitle());		
+		}
 		
 	}
 
