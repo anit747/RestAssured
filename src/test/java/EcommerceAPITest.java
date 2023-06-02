cc9073c28e2981aea6ed8474a8e23c18245e5a26
Anit Rai <anitrai747@gmail.com>
2023-06-02 20:53:28 +0530

    Eccom second commit

--- src/test/java/EcommerceAPITest.java
+++ src/test/java/EcommerceAPITest.java
@@ -1,7 +1,10 @@
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.*;
+
+import java.io.File;
 
 import Pojo.LoginRequest;
 import Pojo.LoginResponse;
@@ -25,6 +28,29 @@ public class EcommerceAPITest {
 			
 			System.out.println(loginResponse.getToken());
 			System.out.println(loginResponse.getUserId());
+			String token = loginResponse.getToken();
+			String userId = loginResponse.getUserId();
+			
+			
+			// Add the product 
+			
+			
+			
+			RequestSpecification addProductBaseReq = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addHeader("Authorization", token).build();
+			
+	 		RequestSpecification	addProduct =	given().log().all().spec(addProductBaseReq).param("productName", "qwerty")
+			.param("productAddedBy", userId).param("productCategory", "fashion").param("productSubCategory", "shirts").param("productPrice", "11500")
+			.param("productDescription", "Addias Originals").param("productFor", "women")
+			.multiPart("productImage",new File ("C:\\Users\\anitr\\Pictures\\Screenshots\\Screenshot 2023-05-21 114406.png")) ;
+	 		
+	 		
+	 	String addProductResponse =	addProduct.when().post("/api/ecom/product/add-product").
+	 		then().log().all().extract().response().asString  ();
+	 	
+	 	JsonPath js = new JsonPath (addProductResponse);
+	 	String productId = js.getString("productId");
+			
+			
 	}
 
 }
